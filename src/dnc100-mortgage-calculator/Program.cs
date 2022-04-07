f1da43139a85eb539072a3a75b84ca1a9b4cf90d
ManleyMan <sweezymac@gmail.com>
2022-04-07 14:56:38 -0700

    completes all exit criteria

--- src/dnc100-mortgage-calculator/Program.cs
+++ src/dnc100-mortgage-calculator/Program.cs
@@ -19,6 +19,7 @@ namespace dnc100_mortgage_calculator
            
             Console.Write("Enter the principal amount: ");
             principal = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"your principal amount is {principal}");
             Console.Write("Enter InterestRate here: ");
             interestRate = Convert.ToDouble(Console.ReadLine());
             Console.Write("Enter term here: ");
@@ -31,7 +32,7 @@ namespace dnc100_mortgage_calculator
              Mortgage mortgage =  new Mortgage(principal, interestRate, term, period);
 
             // Use the Mortgage functions to calculate the monthly payment
-             monthlyPayment = Convert.ToDouble(mortgage);
+             monthlyPayment = mortgage.Calculate(principal,  interestRate,  term,  period);
             // Use WriteLine to output the monthly payment
             Console.WriteLine($"your monthly payment would be {monthlyPayment}");
             Console.ReadLine();
